0287fb7a36b3a17f324a868b98b17d2dc3366df6
MugAbdoul <abdoullatif5027@gmail.com>
2024-04-08 09:31:56 +0200

    Done with array push, shift, unshift and pop

---
+++ Basics/day2.js
@@ -0,0 +1,20 @@
+// Array
+
+const arr = new Array(3,8);
+const arr1 = [3,8,5,6];
+
+arr.push(2);
+arr.push(0);
+arr.push(6);
+
+arr1.push(2);
+arr1.push(0);
+arr1.push(6);
+
+arr.shift()
+arr1.unshift(3)
+arr1.unshift(5)
+// console.log(arr.join(arr1))
+
+console.log(arr);
+console.log(arr1);
